a11480338c5e15e0394598d9b0e7cc59449b2125
Robel <robelseyoum3@gmail.com>
2020-06-09 14:12:24 +0100

    adding new

---
+++ app/src/main/java/com/robelseyoum3/open_api_android_app/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.robelseyoum3.open_api_android_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
